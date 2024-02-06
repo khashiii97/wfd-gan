a7c3d3f5d58964607476b5f97ecd5c40e1a23e2e
khashiii97 <khashiii97@gmail.com>
2024-02-06 11:46:42 -0800

    integrating with cc

--- src/test.py
+++ src/test.py
@@ -1,40 +1,29 @@
-from model import DF, MyConv1dPadSame, MyMaxPool1dPadSame
-from torchsummaryX import summary
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-# df_model = DF(length= 1400)
+import wandb
+import random
 
-# summary(df_model, torch.zeros((32,1400)))
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="my-first-project",
+    
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.02,
+    "architecture": "CNN",
+    "dataset": "CIFAR-100",
+    "epochs": 10,
+    }
+)
 
-
-class TestLayer(nn.Module):
-    def __init__(self):
-        super(TestLayer, self).__init__()
-        self.layer1 = nn.Sequential(
-            MyConv1dPadSame(1, 32, 8, 1),
-            nn.BatchNorm1d(32),
-            nn.ELU(),
-            MyConv1dPadSame(32, 32, 8, 1),
-            nn.BatchNorm1d(32),
-            nn.ELU(),
-            MyMaxPool1dPadSame(8, 1),
-            nn.Dropout(0.1)
-        )
-
-    def forward(self, x):
-        return self.layer1(x)
-
-# Initialize the test layer
-test_layer = TestLayer()
-
-# Create a random input tensor of shape (batch_size, channels, length)
-# For example, a single-channel input vector of length 1000 in a batch of size 1
-input_tensor = torch.rand(1, 1, 1000)
-
-# Apply the test layer to the input tensor
-output_tensor = test_layer(input_tensor)
-
-# Print the shape of the output tensor
-print("Output shape:", output_tensor.shape)
-
+# simulate training
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+    loss = 2 ** -epoch + random.random() / epoch + offset
+    
+    # log metrics to wandb
+    wandb.log({"acc": acc, "loss": loss})
+    
+# [optional] finish the wandb run, necessary in notebooks
+wandb.finish()
