ba72c6ccf69204a20c42fa1f47e7cd1a1413cbd6
khashiii97 <khashiii97@gmail.com>
2024-02-07 14:10:49 -0800

    preparing for compute canada

--- jobs/job.sh
+++ jobs/job.sh
@@ -13,17 +13,18 @@
 #SBATCH --cpus-per-task=4              # Number of CPU cores per task
 #SBATCH --mem=32G                      # Job memory request
 #SBATCH --time=02:00:00                # Time limit hrs:min:sec
-#SBATCH --output=projects/def-t55wang/kka151/wfd-gan/deep_learning_job_%j.log  # Standard output and error log
+#SBATCH --output=/home/kka151/projects/def-t55wang/kka151/wfd-gan/deep_learning_job_%j.log  # Standard output and error log
 #SBATCH --gres=gpu:1                   # Request GPU resource
 
 # Load any modules and activate your conda environment here
 module load python/3.10.2
 module load cuda/11.7
-source venvs/torch/bin/activate
+source /home/kka151/venvs/torch/bin/activate
 
 
 # Navigate to your project directory (optional)
-cd projects/def-t55wang/kka151/wfd-gan/src/
+cd /home/kka151/projects/def-t55wang/kka151/wfd-gan/src/
+
 
 
 # Execute your deep learning script
